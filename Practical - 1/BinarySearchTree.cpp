6f3346906a485c5f83f82d595e060f01aed4c456
Siddhant Kumar Sahu <136563394+siddsahu17@users.noreply.github.com>
2025-01-15 09:06:25 +0530

    add files

--- Practical - 1/BinarySearchTree.cpp
+++ Practical - 1/BinarySearchTree.cpp
@@ -30,6 +30,7 @@ Tree* Insert(Tree* Root, int a){
     return Root;
 }
 
+// Recursive Calling
 void InOrder(Tree* Root){
     if(Root == NULL){
         return;
@@ -60,6 +61,69 @@ void PostOrder(Tree* Root){
     cout << Root->data << " ";
 }
 
+#define N 100
+
+// class Stack{
+// //	implmentation in either linked list or array
+// 	public:
+// 		string s1[N];
+// 		int top;
+// 		Stack(){
+// 			top == -1;
+// 		}
+// 		bool isFull(){
+// 			if(top == N-1){
+// 				return true;
+// 			}else{
+// 				return false;
+// 			}
+// 		}
+		
+// 		bool isEmpty(){
+// 			if(top == -1){
+// 				return true;
+// 			}else{
+// 				return false;
+// 			}
+// 		}
+	
+// 		void push(int s){
+// 			if(isFull()){
+// 				cout << "Insertion not possible \nOverflow";
+// 			}else{
+// 				top = top+1;
+// 				s1[top] = s;
+// 			}
+// 		}
+		
+// 		string pop(string s){
+// 			if(isEmpty()){
+// 				cout << "Stack is Empty\nPop not possible";
+// 			}else{
+// 				string temp = s1[top];
+// 				top = top-1;
+// 				return temp;
+// 			}
+// 		}	
+// };
+
+// Non Recursive Calling
+void InOrderNR(Tree* Root){
+    Tree *curr = Root;
+    stack<Tree*> s;
+    while(curr != NULL || s.empty() == false){
+        while(curr != NULL){
+            s.push(curr);
+            curr = curr->left;
+        }
+        curr = s.top();
+        s.pop();
+
+        cout << curr->data << " ";
+        curr = curr->right;
+    }
+}
+
 int main(){
     Tree* Root = NULL;
     Root = Insert(Root,45);
@@ -81,6 +145,10 @@ int main(){
 
     cout << "Post-Order Traversal of the TREE" << endl;
     PostOrder(Root);
+    cout << endl;
+
+    cout << "IN-Order NON RECURSIVE Traversal of the TREE" << endl;
+    InOrderNR(Root);
     cout << endl;
 
     return 0;
